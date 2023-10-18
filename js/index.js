18dff8d90a9e6710d959847cb77df64069bc3110
guilhermesenadr <127293550+guilhermesenadr@users.noreply.github.com>
2023-10-18 06:13:48 +0100

    atualizando o projeto
    mudando algumas coisas

--- js/index.js
+++ js/index.js
@@ -1,16 +1,46 @@
-const acorde=document.querySelector("#idnota")
-const strofe=document.querySelector("#idletra")
-const btnacorde=document.querySelector("#btnNota")
-const btnstrofe=document.querySelector("#btnStrofe")
-const section=document.querySelector("#section")
-const article=document.querySelector("#article")
+const caixa=document.querySelector("#caixa")
+const titulo=document.querySelector("#titulo")
+const cantor=document.querySelector("#cantor")
+const trecho=document.querySelector("#trecho")
+const nomeTitulo=document.querySelector("#texto_titulo")
+const BtnNovo=document.querySelector("#btnNovaMusica")
+const conteudo=document.querySelector("#conteudo")
+const textoh3titulo=document.querySelector("nome_titulo")
+const textoh3cantor=document.querySelector("texto_cantor")
+const textoh3trecho=document.querySelector("texto_trecho")
 
-btnacorde.addEventListener("click", ()=>{
-    console.log(idnota.value)
-    section.appendChild("div", )
+let i=0
+const novoCaixa=document.createElement("div")
+novoCaixa.setAttribute("id", "caixa"+i)
+novoCaixa.setAttribute("class", "elemusica")
     
+const novoTitulo=document.createElement("div")
+novoTitulo.setAttribute("id", "titulo")
+novoTitulo.setAttribute("class", "containero")
+const novoHTitulo=document.createElement("h1")
+novoHTitulo.innerHTML="Titulo:"
+const novoHnome=document.createElement("h3")
+novoHnome.setAttribute("id","nome_titulo" )
+
+const novoCantor=document.createElement("div")
+novoCantor.setAttribute("id", "cantor")
+novoCantor.setAttribute("class", "containero")
+const novoHcantor=document.createElement("h1")
+novoHTitulo.innerHTML="Titulo:"
+const novoHnomecantor=document.createElement("h3")
+novoHnomecantor.setAttribute("id","nome_cantor" )
+
+
+
+
+BtnNovo.addEventListener("click", (ev)=>{
+   const inputSelecionado=nomeTitulo
+   const inputCantor=
+   conteudo.insertBefore(novoCaixa,inputSelecionado)
+   i=i++
+   novoCaixa.appendChild(novoTitulo)
+   novoTitulo.appendChild(novoHTitulo)
+   novoTitulo.appendChild(novoHnome)
+   novoHnome.innerHTML=inputSelecionado.value
+
 })
-btnstrofe.addEventListener("click", ()=>{
-    
-})
-    
